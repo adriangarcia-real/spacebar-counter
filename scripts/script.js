7ee0fb4ca110b9f3a867cc1bffdb3e8f97ade0ed
Adrian <adriangarcia8382@gmail.com>
2023-08-27 13:14:02 +0800

    Added a reset button to reset the counter

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,18 @@
 const counterElement = document.getElementById("counter");
+const resetButton = document.getElementById("reset-button");
 
 let counter = 0;
-
 function incrementCounter(e) {
   if (!(e.code === "Space")) return;
   counter += 1;
   counterElement.textContent = counter;
 }
 
+function resetCounter(e) {
+  resetButton.blur();
+  counter = 0;
+  counterElement.textContent = counter;
+}
+
 window.addEventListener("keyup", incrementCounter);
+resetButton.addEventListener("click", resetCounter);
