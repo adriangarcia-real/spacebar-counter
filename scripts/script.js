5bf8411287e5e8f1a3e4b3c014ba538c0df056df
Adrian <adriangarcia8382@gmail.com>
2023-08-27 13:38:38 +0800

    Added sound effects when clicking space and resetting counter

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,18 @@
 const counterElement = document.getElementById("counter");
 const resetButton = document.getElementById("reset-button");
+const clickSound = document.getElementById("click");
+const resetSound = document.getElementById("reset");
 
 let counter = 0;
 function incrementCounter(e) {
   if (!(e.code === "Space")) return;
+  clickSound.play();
   counter += 1;
   counterElement.textContent = counter;
 }
 
 function resetCounter(e) {
+  resetSound.play();
   resetButton.blur();
   counter = 0;
   counterElement.textContent = counter;
